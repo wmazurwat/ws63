b5d1fee0a477352581690b174cc8967587e58233
Wojciech Mazur <wojciech.mazur.lo@gmail.com>
2022-06-19 13:38:21 +0200

    update 3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
-import logo from "./logo.svg";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Capture from "./components/Capture";
+import logo from "./logo.svg";
 
 function App() {
+  const [file, setFile] = useState();
+  const [preview, setPreview] = useState();
+  const [text1, setText1] = useState("Text1");
+  const onFileInputChange = (event) => {
+    setFile(event.target.files[0]);
+  };
+  const onTextInputChange = (event) => {
+    setText1(event.target.value);
+  };
+  useEffect(() => {
+    if (!file) {
+      setPreview(undefined);
+      return;
+    }
+
+    const objectUrl = URL.createObjectURL(file);
+    setPreview(objectUrl);
+    // free memory when ever this component is unmounted
+    return () => URL.revokeObjectURL(objectUrl);
+  }, [file]);
   return (
     <div className="App">
       <header className="App-header">
-        <Capture img={logo} />
+        {preview && (
+          <Capture img={preview} textTop={"Kiedy nie lubisz Reacta"} textBottom={"ale Jacek wyszkolił cię w jeden księżyc"} />
+        )}
+        <input type={"file"} onChange={onFileInputChange} />
+        <input type={"text"} onChange={onTextInputChange} />
       </header>
     </div>
   );
--- src/components/Capture.js
+++ src/components/Capture.js
@@ -1,44 +1,43 @@
+import React, { useCallback, useRef } from "react";
 import html2canvas from "html2canvas";
-import React, { useRef } from "react";
+const exportAsImage = async (el, imageFileName) => {
+  const canvas = await html2canvas(el);
+  const image = canvas.toDataURL("image/png", 1.0);
+  downloadImage(image, imageFileName);
+};
+const downloadImage = (blob, fileName) => {
+  const fakeLink = window.document.createElement("a");
+  fakeLink.style = "display:none;";
+  fakeLink.download = fileName;
 
-const Capture = ({
-  img,
-  textTop = "text top",
-  textBottom = "bottom tekst",
-}) => {
+  fakeLink.href = blob;
+
+  document.body.appendChild(fakeLink);
+  fakeLink.click();
+  document.body.removeChild(fakeLink);
+
+  fakeLink.remove();
+};
+
+const Capture = ({ img, textTop, textBottom }) => {
   const ref = useRef(null);
-  const exportAsImage = async (el, imageFileName) => {
-    const canvas = await html2canvas(el);
-    const image = canvas.toDataURL("image/png", 1.0);
-    downloadImage(image, imageFileName);
-  };
-  const downloadImage = (blob, fileName) => {
-    const fakeLink = window.document.createElement("a");
-    fakeLink.style = "display:none;";
-    fakeLink.download = fileName;
 
-    fakeLink.href = blob;
-
-    document.body.appendChild(fakeLink);
-    fakeLink.click();
-    document.body.removeChild(fakeLink);
-
-    fakeLink.remove();
-  };
-  const divStyle = {
-    backgroundImage: `url(${img})`,
-    backgroundSize: "cover",
-    position: "relative",
-    width: 400,
-    height: 600,
-    backgroundRepeat: "no-repeat",
-    display: "flex",
-    justifyContent: "space-between",
-    flexDirection: "column",
-  };
   return (
     <>
-      <div ref={ref} style={divStyle}>
+      <div
+        ref={ref}
+        style={{
+          backgroundImage: `url(${img})`,
+          backgroundSize: "cover",
+          position: "relative",
+          width: 400,
+          height: 600,
+          backgroundRepeat: "no-repeat",
+          display: "flex",
+          justifyContent: "space-between",
+          flexDirection: "column",
+        }}
+      >
         <p style={{ zIndex: 1 }}>{textTop}</p>
         <p style={{ zIndex: 1 }}>{textBottom}</p>
       </div>
